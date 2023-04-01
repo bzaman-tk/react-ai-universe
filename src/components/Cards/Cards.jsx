0b226e57a3b1d41674d3780f7047b0a0f1974776
bzaman_tk <bzaman.tk@gmail.com>
2023-04-02 00:51:24 +0600

    previus modal data fliker for a secend

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,12 +4,12 @@ import SingleCard from '../SingleCard/SingleCard';
 
 const Cards = () => {
     const [data, setData] = useState([]);
-    const [showAll, setShowAll] = useState(false);
-    const [modalId, setModalId] = useState(null);
-    const [modalData, setModalData] = useState(null);
+    const [showAll, setShowAll] = useState(false); //for show all DATA
+    const [modalId, setModalId] = useState(null); //Store card id Each after click
+    const [modalData, setModalData] = useState(null); //Store card data by each id
 
-    console.log(modalData)
-
+    // console.log(modalData)
+    //load signle card data for modal (based on modal id on state if avaiable)
     useEffect(() => {
         if (modalId) {
             fetch(`https://openapi.programming-hero.com/api/ai/tool/${modalId}`)
@@ -21,15 +21,17 @@ const Cards = () => {
                 .then(data => setModalData(data));
         }
     }, [modalId])
-
+    //to get all data at first load
     useEffect(() => {
         fetch('https://openapi.programming-hero.com/api/ai/tools')
             .then(res => res.json())
             .then(data => setData(data.data.tools));
     }, []);
+    //to handle show all cards or not
     const handleShowAll = () => {
         setShowAll(true);
     }
+
     return (
         <>
             <button className="btn">Sort by Date</button>
@@ -37,6 +39,7 @@ const Cards = () => {
                 {data.slice(0, showAll ? 12 : 6).map(x => <SingleCard key={x.id} data={x} setModalId={setModalId} />)}
             </div>
             {showAll || (<button onClick={handleShowAll} className="btn">Show all</button>)}
+            {/* render if modal data is set */}
             {modalData ? <Modal modalData={modalData}></Modal> : " "}
         </>
     );
